8ca33ef373ef4b5a22bf63c3e95788f1f37654d1
bembelem <nikvedd@gmail.com>
2025-11-25 21:33:44 +1000

    feat: get_game method

--- backend/database/repositories/game.py
+++ backend/database/repositories/game.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session
 from backend.database.models import Game
 from sqlalchemy import select, ScalarResult
 from backend.database.connection import SessionLocal
+from backend.database.models import Offer
 
 
 class GameRepository:
@@ -13,12 +14,21 @@ class GameRepository:
         result = self.session.execute(stmt).scalars().all()
         return result
 
+    def get_game(self, game_id: int):
+        # stmt = (
+        #     select(Offer)
+        #     .join(Game, Game.offer_id == Offer.id)
+        #     .where(Game.id == game_id)
+        # )
+        stmt = select(Game).where(Game.id==game_id)
+        result = self.session.execute(stmt).scalar()
+        return result
+
     def count(self):
         from sqlalchemy import func
         return self.session.execute(select(func.count(Game.id))).scalar()
 
 
-
 # session = SessionLocal()
 # game_repository = GameRepository(session)
-# print(game_repository.get_page())
+# print(game_repository.get_game(1))
