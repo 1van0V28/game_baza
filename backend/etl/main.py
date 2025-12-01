a21a3cefcba8b9f2ed4bee5b59415e7a92b358b3
bembelem <nikvedd@gmail.com>
2025-12-01 13:42:24 +1000

    feature: add main.py to etl module

--- backend/etl/main.py
+++ backend/etl/main.py
@@ -1,21 +1,28 @@
-from json_loader import JsonImporter
+from backend.database.connection import SessionLocal
 from pathlib import Path
-from backend.database.connection import session
-from deduplicator import Deduplicator
+from json_loader.json_loader import JsonLoader
+from deduplicator.deduplicator import Deduplicator
 
-GAMES_JSONL_PATH = Path(__file__).resolve().parents[1] / "etl" / "games.jsonl"
+GAMES_JSONL_PATH = Path(__file__).resolve().parent / 'deduplicator' / "games.jsonl"
 
 STEAM_JSONL_PATH = Path(__file__).resolve().parents[1] / "parsers" / "steam_games.jsonl" # Отсюда берется вся основная информация для games
-GABESTORE_JSONL_PATH = Path(__file__).resolve().parents[1] / "parsers" / "gabestore.jsonl"
+GABESTORE_JSONL_PATH = Path(__file__).resolve().parents[1] / "parsers" / "gabestore_games.jsonl"
 STEAMBUY_JSONL_PATH = Path(__file__).resolve().parents[1] / "parsers" / "steambuy_games.jsonl"
 
-store_jsonl_paths = {
-    "Steam": STEAMBUY_JSONL_PATH,
-    "GabeStore": GABESTORE_JSONL_PATH,
-    "SteamBuy": STEAMBUY_JSONL_PATH
+stores_jsonl_paths = {
+    'Steam': STEAMBUY_JSONL_PATH,
+    'GabeStore': GABESTORE_JSONL_PATH,
+    'SteamBuy': STEAMBUY_JSONL_PATH,
 }
 
-json_importer = JsonImporter(session)
+def import_games_to_database():
+    deduplicate_offers()
+    json_loader = JsonLoader(SessionLocal)
+    json_loader.load_data(GAMES_JSONL_PATH, stores_jsonl_paths)
 
-deduplicator = Deduplicator(store_jsonl_paths)
-deduplicator.start_dedupe()
+def deduplicate_offers():
+    deduplicator = Deduplicator(stores_jsonl_paths)
+    deduplicator.start_dedupe()
+
+deduplicate_offers()
+import_games_to_database()
