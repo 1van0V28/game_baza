479edd0d58b711145956f29679580e6e6ce56429
bembelem <nikvedd@gmail.com>
2025-12-01 17:49:06 +1000

    refactor: move core.py to init_db.py

---
+++ backend/database/init_db.py
@@ -0,0 +1,15 @@
+from backend.database.connection import engine
+from backend.database.models import Base
+# from sqlalchemy.schema import CreateTable
+# from pathlib import Path
+
+# SCHEMA_PATH = Path(__file__).resolve().parents[1] / "schemas" / "schema.sql"
+
+def create_tables():
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+
+    # with open(SCHEMA_PATH, "w", encoding="utf-8") as f:
+    #     for table in Base.metadata.sorted_tables:
+    #         f.write(str(CreateTable(table).compile(engine)))
+    #         f.write(";\n\n")
