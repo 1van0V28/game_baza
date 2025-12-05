b308444900237a1d86d3ce58b834a98d9225b1d1
bembelem <nikvedd@gmail.com>
2025-12-05 18:47:42 +1000

    feature: add filtration, handlers

--- backend/api/main.py
+++ backend/api/main.py
@@ -2,15 +2,15 @@ from fastapi import FastAPI, Depends, HTTPException, Query
 from typing import Optional, List
 from sqlalchemy.orm import Session
 
-from schemas import (
+from backend.api.schemas import (
     GamesPage,
     GamePreview,
     GameFull,
     OfferDTO,
+    GameFilters,
 )
-from backend.database.repositories.repositories import GameRepository
-from backend.database.connection import get_db
-from backend.api.filters import GameFilters
+from backend.database.repositories.repositories import GameRepository, GenreRepository, StoreRepository
+from backend.database.connection import get_session
 
 app = FastAPI()
 
@@ -29,23 +29,22 @@ def compute_min_prices(offers: List[OfferDTO]):
 
     return min_price_original, min_price_discount, discount_percent
 
-
 @app.get("/games", response_model=GamesPage)
 def get_games(
     last_id: int = 0,
     per_page: int = 50,
     title: Optional[str] = None,
-    tags: Optional[List[str]] = Query(None),
     platforms: Optional[List[str]] = Query(None),
     genres: Optional[List[str]] = Query(None),
+    stores: Optional[List[str]] = Query(None),
     price_min: Optional[int] = None,
     price_max: Optional[int] = None,
     sort: Optional[str] = None,
-    db: Session = Depends(get_db),
+    db: Session = Depends(get_session),
 ):
     filters = GameFilters(
         title=title,
-        tags=tags,
+        stores=stores,
         platforms=platforms,
         genres=genres,
         price_min=price_min,
@@ -97,9 +96,8 @@ def get_games(
         items=items,
     )
 
-
 @app.get("/games/{game_id}", response_model=GameFull)
-def get_game(game_id: int, session: Session = Depends(get_db)):
+def get_game(game_id: int, session: Session = Depends(get_session)):
     repo = GameRepository(session)
     game = repo.get_game(game_id)
 
@@ -133,45 +131,17 @@ def get_game(game_id: int, session: Session = Depends(get_db)):
         title=game.title,
     )
 
+@app.get("/genres")
+def get_genres(db: Session = Depends(get_session)):
+    repo = GenreRepository(db)
+    genres = repo.get_genres()
+    return genres
 
-@app.get("/games/search", response_model=GamesPage)
-def search_games(query: str, db: Session = Depends(get_db)):
-    repo = GameRepository(db)
-    games = repo.search_by_title(query)
-
-    items = []
-
-    for game in games:
-        offer_dtos = [
-            OfferDTO(
-                price_original=o.price_original,
-                price_discount=o.price_discount,
-                store=o.store.name,
-                store_game_link=o.store_game_link
-            )
-            for o in game.offers
-        ]
-
-        min_price_original, min_price_discount, discount_percent = compute_min_prices(offer_dtos)
-
-        items.append(
-            GamePreview(
-                id=game.id,
-                title=game.title,
-                image_url=game.image_url,
-                min_price_original=min_price_original,
-                min_price_discount=min_price_discount,
-                discount_percent=discount_percent,
-            )
-        )
-
-    return GamesPage(
-        total=len(items),
-        last_id=None,
-        per_page=len(items),
-        items=items,
-    )
-
+@app.get("/stores")
+def get_genres(db: Session = Depends(get_session)):
+    repo = StoreRepository(db)
+    stores = repo.get_stores()
+    return stores
 
 if __name__ == "__main__":
     import uvicorn
